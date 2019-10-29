9c34c0ec27c590748ef697ba00587c8b934a5a9a
Victor Rono <v.rono@icloud.com>
2019-10-29 14:21:58 -0500

    id of voice selected

--- main.js
+++ main.js
@@ -4,3 +4,4 @@ const synth = window.SpeechSynthesis;
 //DOM elements
 const textForm = document.querySelector('form');
 const textInput = document.querySelector('#text-input');
+const voiceSelect = document.querySelector('#voice-select');
